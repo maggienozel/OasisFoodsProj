29a9a05a4f0d9516a51269df31c314c262527ffe
Maggie Nozel <maggie.nozel@marquette.edu>
2023-11-28 12:55:30 -0600

    Add project files.

---
+++ OasisFoodsProj/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace OasisFoodsProj
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
